cd8489ef59d5284127cf0c622130b3afb25478f7
Lev Filippov <filippov.l.a.89@gmail.com>
2023-03-21 22:30:48 +0800

    Дописал алгоритм

--- Program.cs
+++ Program.cs
@@ -3,19 +3,35 @@
 public class Program {
     public static void Main(){
         var initialString = getArgsFromConsole();
-        Console.WriteLine($"Исходная строкаданных {initialString}");
-        
+        var delim = 3;
+        Console.WriteLine($"Исходная строка данных {initialString}");
+
+        String[] initialArray = initialString.Split("$");
+        Console.WriteLine("Делим на токены");
+        String[] finalArray = new String[initialArray.Length];
+        long counter = 0;
+        Console.WriteLine("Начинаем работу в цикле for");
+        for(int i = 0; i<initialArray.Length; i++){
+            if(initialArray[i].Length <= delim) {
+                finalArray[counter] = initialArray[i];
+                counter++;
+            }
+        }
+        Console.WriteLine($"Работа в цикле for окончена, размер финального массива {counter}");
+        String[] result = new String[counter];
+        Array.Copy(finalArray, result, counter);
+        Console.WriteLine(String.Join(" ",result));
     }
 
     private static String getArgsFromConsole(){
-        Console.WriteLine("Введите произвольное колечество слов/символов, разделяя их клавишей ввод. Когда надоест - ваберите слово \"хватит\".");
+        Console.WriteLine("Введите произвольное количество слов/символов, разделяя их клавишей ввод. Когда надоест - ваберите слово \"хватит\". Не применяйте символ $");
         StringBuilder sb = new StringBuilder();
         string ? token; 
         while(true){
             token = Console.ReadLine() ?? "";
             if(token.ToLowerInvariant().Equals("хватит"))
                 break;
-            sb.Append(token).Append(" ");
+            sb.Append(token).Append("$");
             Console.WriteLine($"Принято слово {token}");
         }    
         return sb.ToString();
